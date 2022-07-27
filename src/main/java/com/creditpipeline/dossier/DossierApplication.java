345a5e139f3299ac971b1e6717a2cfb68087a265
Eugene Fom <Eugene_fom@mail.ru>
2022-07-27 18:46:14 +0300

    4.1.1. Инициализировать Spring-Boot микросервис dossier

---
+++ src/main/java/com/creditpipeline/dossier/DossierApplication.java
@@ -0,0 +1,11 @@
+package com.creditpipeline.dossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DossierApplication.class, args);
+    }
+}
